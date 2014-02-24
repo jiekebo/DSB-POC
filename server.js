e67193b391cc4d5e13d7531d3020758384aa0290
Jacob Salomonsen <jiekebo@gmail.com>
2014-02-24 19:34:07 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var js = require('jaydata');
+var request = require('request');
+
+var queryString = ("http://traindata.dsb.dk/stationdeparture/opendataprotocol.svc/Queue()?$filter=TrainType ne 'S-Tog'");
+var queryStations = ("http://traindata.dsb.dk/stationdeparture/opendataprotocol.svc/Station()?$filter=CountryCode eq 'DK'");
+
+var main = function () {
+	request({
+			url: queryStations,
+			json: true
+		},
+		function (error, response, body) {
+			console.log(body);
+		});
+};
+
+if(require.main === module) {
+	main();
+}
